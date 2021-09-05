d09b2f8f200b3a938d5f27afba7308d8fe499ad9
PC <khusanov.it.tech@gmail.com>
2021-09-05 10:56:14 +0500

    bug-fix (mobile version swiper - ingridients)

--- 9. The Night Treasure/app/js/scripts/ingridients.js
+++ 9. The Night Treasure/app/js/scripts/ingridients.js
@@ -5,38 +5,40 @@ const featureCardsContainer = document.getElementById("feature__cards"),
   featureRightBtn = document.getElementById("feature-right"),
   featureCards = document.querySelectorAll(".feature__card");
 
-let idx = 0;
+if (document.documentElement.clientWidth <= 400) {
+  let idx = 0;
 
-let interval = setInterval(run, 2000);
+  let interval = setInterval(run, 2000);
 
-function run() {
-  idx++;
-  changeImage();
-}
-
-function changeImage() {
-  if (idx > featureCards.length - 1) {
-    idx = 0;
-  } else if (idx < 0) {
-    idx = featureCards.length - 1;
+  function run() {
+    idx++;
+    changeImage();
   }
 
-  featureCardsContainer.style.transform = `translateX(${-idx * 50}rem)`;
+  function changeImage() {
+    if (idx > featureCards.length - 1) {
+      idx = 0;
+    } else if (idx < 0) {
+      idx = featureCards.length - 1;
+    }
+
+    featureCardsContainer.style.transform = `translateX(${-idx * 50}rem)`;
+  }
+
+  function resetInterval() {
+    clearInterval(interval);
+    interval = setInterval(run, 2000);
+  }
+
+  featureRightBtn.addEventListener("click", () => {
+    idx++;
+    changeImage();
+    resetInterval();
+  });
+
+  featureLeftBtn.addEventListener("click", () => {
+    idx--;
+    changeImage();
+    resetInterval();
+  });
 }
-
-function resetInterval() {
-  clearInterval(interval);
-  interval = setInterval(run, 2000);
-}
-
-featureRightBtn.addEventListener("click", () => {
-  idx++;
-  changeImage();
-  resetInterval();
-});
-
-featureLeftBtn.addEventListener("click", () => {
-  idx--;
-  changeImage();
-  resetInterval();
-});
